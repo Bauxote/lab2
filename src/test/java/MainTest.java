536dff0af4c41f20d2baef531091d1a34c14be8f
Bauxote <east175@proton.me>
2026-04-20 15:13:46 +0300

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,1 +1,43 @@
+import org.junit.Assert;
+import org.junit.Test;
 
+import main.java.Main;
+
+public class MainTest {
+
+    @Test
+    public void testCompareEqual() {
+        StringBuffer sb1 = new StringBuffer("apple");
+        StringBuffer sb2 = new StringBuffer("apple");
+        Assert.assertEquals(0, Main.compare(sb1, sb2));
+    }
+
+    @Test
+    public void testCompareFirstSmaller() {
+        StringBuffer sb1 = new StringBuffer("apple");
+        StringBuffer sb2 = new StringBuffer("banana");
+        Assert.assertTrue(Main.compare(sb1, sb2) < 0);
+    }
+
+    @Test
+    public void testCompareFirstLarger() {
+        StringBuffer sb1 = new StringBuffer("banana");
+        StringBuffer sb2 = new StringBuffer("apple");
+        Assert.assertTrue(Main.compare(sb1, sb2) > 0);
+    }
+
+    @Test
+    public void testCompareSamePrefixShorterFirst() {
+        StringBuffer sb1 = new StringBuffer("cat");
+        StringBuffer sb2 = new StringBuffer("cats");
+        Assert.assertTrue(Main.compare(sb1, sb2) < 0);
+    }
+
+    @Test
+    public void testCompareSamePrefixLongerFirst() {
+        StringBuffer sb1 = new StringBuffer("cats");
+        StringBuffer sb2 = new StringBuffer("cat");
+        Assert.assertTrue(Main.compare(sb1, sb2) > 0);
+
+}
+    
